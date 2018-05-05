cdd843999e51d4051513f207050b2cf726a9c456
Arundhati <arundhati.u.sawant@gmail.com>
2018-05-05 12:46:15 -0700

    Add priority to transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -5,21 +5,24 @@ import java.util.Date;
 public class Transaction {
     private Timestamp timeStamp;
     private String transaction;
+    private StatementPriority statementPriority;
 
-    public Transaction(Timestamp timeStamp, String transaction) {
+    public Transaction(Timestamp timeStamp, String transaction, StatementPriority statementPriority) {
         this.timeStamp = timeStamp;
         this.transaction = transaction;
+        this.statementPriority = statementPriority;
     }
 
-    public Date getDate() {
+    public Date getTime() {
         return new Date(this.timeStamp.getTime());
     }
 
     @Override
     public String toString() {
         return "Transaction{" +
-                "timeStamp='" + timeStamp + '\'' +
+                "timeStamp=" + timeStamp +
                 ", transaction='" + transaction + '\'' +
+                ", statementPriority=" + statementPriority +
                 '}';
     }
 
