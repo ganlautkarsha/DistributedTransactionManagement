c5852d6ddee0d1b116c14af4a3a0b9524142e764
Tushar Kulkarni <tkul@outlook.com>
2018-05-10 17:10:18 -0700

    Pre-Processing

--- src/TransactionPreProcessing.java
+++ src/TransactionPreProcessing.java
@@ -69,7 +69,32 @@ public class TransactionPreProcessing {
             i.printStackTrace();
         }
 
+//        ---- For reading TreeMap.ser ----
+//        TreeMap<String,List<String>> allOperations;
+//        try {
+//            FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+//            ObjectInputStream in = new ObjectInputStream(fileIn);
+//            allOperations = (Employee) in.readObject();
+//            in.close();
+//            fileIn.close();
+//        } catch (IOException i) {
+//            i.printStackTrace();
+//            return;
+//        } catch (ClassNotFoundException c) {
+//            System.out.println("Employee class not found");
+//            c.printStackTrace();
+//            return;
+//        }
+//
+//        System.out.println("Deserialized Employee...");
+//        System.out.println("Name: " + e.name);
+//        System.out.println("Address: " + e.address);
+//        System.out.println("SSN: " + e.SSN);
+//        System.out.println("Number: " + e.number);
     }
+
+
+}
 
 
     //use same for semantic file
